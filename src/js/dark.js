745f9a1e091e0a4da5bb1966ca90b9c2b077c7d5
xiejiatree <47264339+xiejiatree@users.noreply.github.com>
2022-02-27 01:53:24 -0500

    Add files via upload

---
+++ src/js/dark.js
@@ -0,0 +1,45 @@
+function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    var items = location.search.substr(1).split("&");
+    for (var index = 0; index < items.length; index++) {
+        tmp = items[index].split("=");
+        if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+    }
+    return result;
+}
+
+function toggleDark() {
+    var darkMode = document.getElementsByClassName('dark-mode');
+    darkMode[0].classList.toggle('show');
+    var lightMode = document.getElementsByClassName('light-mode');
+    lightMode[0].classList.toggle('show');
+
+    var light = document.getElementsByClassName('lightback');
+    
+    if (darkMode[0].className.match(/(?:^|\s)show(?!\S)/)) {
+        for (var index = 0; index < light.length; index++)
+            light[index].className = light[index].className.replace( /(?:^|\s)darkback(?!\S)/g , '' );
+        document.getElementsByClassName('mainhead')[0].className = document.getElementsByClassName('mainhead')[0].className.replace( /(?:^|\s)darkback(?!\S)/g , '' );
+        document.getElementById('titlename').className = document.getElementById('titlename').className.replace( /(?:^|\s)darkback(?!\S)/g , '' );
+        portfolioPins = document.getElementsByClassName('portfolio-pin');
+        for (var index = 0; index < portfolioPins.length; index++) {
+            portfolioPins[index].src = portfolioPins[index].src.replace( '&theme=tokyonight', '' );
+        }
+    }
+    else {
+        for (var index = 0; index < light.length; index++)
+            light[index].className += ' darkback';
+        document.getElementsByClassName('mainhead')[0].className += ' darkback';
+        document.getElementById('titlename').className += ' darkback';
+        portfolioPins = document.getElementsByClassName('portfolio-pin');
+        for (var index = 0; index < portfolioPins.length; index++) {
+            portfolioPins[index].src += '&theme=tokyonight';
+        }
+    }
+
+}
+
+var hour = new Date().getHours();
+if (hour <= 5 || hour >= 20)
+    toggleDark();
---
+++ src/js/profile.js
@@ -0,0 +1,22 @@
+const textModules = document.getElementsByClassName('text-module');
+const textLines = document.getElementsByClassName('subdescrip');
+
+function setModule(setting) {
+    for (txt of textLines) {
+        if (txt.id == setting) txt.style.display = 'block';
+        else txt.style.display = 'none';
+    }
+}
+
+textModules[0].style.borderStyle = 'solid';
+setModule('personals');
+
+for (let index=0; index<textModules.length; index++) {
+    let mod = textModules[index];
+    mod.addEventListener('click', () => {
+        for (other of textModules) other.style.borderStyle = 'hidden';
+        mod.style.borderStyle = 'solid';
+        setModule(mod.id.split('-')[0]);
+    });
+    mod.addEventListener('mouseover', () => mod.style.cursor = 'pointer');
+}
